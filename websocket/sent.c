09e0db3f6ab4cf844976b98d6577687dd15ef2a7
Misso <mishiro.24@gmail.com>
2015-03-16 10:57:35 +0900

    Create sent.c

---
+++ websocket/sent.c
@@ -0,0 +1,42 @@
+/* sent.c 
+ * 
+ * 日時を1秒毎にFIFO(名前付きパイプ)に送出する */
+
+
+#include        <stdio.h>
+#include        <stdlib.h>
+#include        <string.h>
+#include        <sys/types.h>
+#include        <sys/stat.h>
+#include        <sys/errno.h>
+#include        <sys/fcntl.h>
+#include        <time.h>
+
+
+void main()
+{
+int fd;
+char buf[256];
+time_t timer;
+
+        if(mkfifo("FifoTest",0666)==-1){
+                perror("mkfifo");
+        }
+
+        if((fd=open("FifoTest",O_WRONLY))==-1){
+                perror("open");
+                exit(-1);
+        }
+
+        while(1){
+                timer = time(NULL);
+                sprintf(buf, "%s" , ctime(&timer));
+//              fgets(buf,sizeof(buf)-1,stdin);
+//              if(feof(stdin)){
+//                      break;
+//              }
+                write(fd,buf,strlen(buf));
+                sleep(1);
+        }
+        close(fd);
+}
