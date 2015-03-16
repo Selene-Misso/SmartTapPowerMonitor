2e3ecd4d327a88531864b44c3746b221c7de30ba
Misso <mishiro.24@gmail.com>
2015-03-17 02:30:21 +0900

    FIFOから得た値をJSON形式に変換し，配信するようにした．

--- websocket/multicst.rb
+++ websocket/multicst.rb
@@ -4,15 +4,37 @@
 # FIFO -> JSON -> Websocket -> All User
 
 require 'em-websocket'
+require 'json'
 
 EventMachine.run {
   @channel = EM::Channel.new
+
+  @row = {}
+  @outlet1 = {"power(W)" => 0, "name" => "none"}
+  @outlet2 = {"power(W)" => 0, "name" => "none"}
+  @outlet3 = {"power(W)" => 0, "name" => "none"}
+  @outlet4 = {"power(W)" => 0, "name" => "none"}
+  @row["date"]    = Time.now
+  @row["outlet1"] = @outlet1
+  @row["outlet2"] = @outlet2
+  @row["outlet3"] = @outlet3
+  @row["outlet4"] = @outlet4
 
   # Thread monitoring FIFO
   read_thread = Thread.new do
     @fifo = open("FifoTest", "r")
     @fifo.each do |line|
-      @channel.push "Time: #{line}."
+      line.chomp!
+      lineAry = line.split(" ", 4)
+      
+      @row["date"]         = Time.now
+      @outlet1["power(W)"] = lineAry[0].to_i
+      @outlet2["power(W)"] = lineAry[1].to_i
+      @outlet3["power(W)"] = lineAry[2].to_i
+      @outlet4["power(W)"] = lineAry[3].to_i
+      
+      puts JSON.pretty_generate(@row)
+      @channel.push JSON.generate(@row)
     end
   end
 
